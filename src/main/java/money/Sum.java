2e8c43072473eabaea5f141ddf6a399a812a87c7
r-miyamae <ryohei_miyamae@voyagegroup.com>
2019-06-10 11:06:31 +0900

    complete chapter16

--- src/main/java/money/Sum.java
+++ src/main/java/money/Sum.java
@@ -3,12 +3,15 @@ package money;
 class Sum implements Expression{
     Expression augend;
     Expression addend;
+    public Expression times(int multiplier) {
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
+    }
     Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
     public Expression plus(Expression addend) {
-        return null;
+        return new Sum(this, addend);
     }
     public Money reduce(Bank bank, String to) {
         int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
